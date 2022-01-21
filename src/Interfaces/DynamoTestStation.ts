d981ef9646a8f426cabd91e55ee62065c97dcea8
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-01-21 16:13:41 +0000

    feat(cb2-2097): added test station interfaces

---
+++ src/Interfaces/DynamoTestStation.ts
@@ -0,0 +1,17 @@
+interface DynamoTestStation {
+  testStationId : string,
+  testStationAddress: string,
+  testStationContactNumber: string,
+  testStationEmails: string,
+  testStationGeneralNotes: string,
+  testStationLongitude: string,
+  testStationLatitude: string,
+  testStationName: string,
+  testStationPNumber: string,
+  testStationPostcode: string,
+  testStationStatus: string,
+  testStationTown: string,
+  testStationType: string
+}
+
+export { DynamoTestStation };
