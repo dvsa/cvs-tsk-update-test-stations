e00bed8e7c944fd41fbd5beb8adeb81350ad8eb3
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-01-21 16:44:03 +0000

    feat(cb2-2097): added accessNotes

--- src/Interfaces/DynamoTestStation.ts
+++ src/Interfaces/DynamoTestStation.ts
@@ -1,17 +1,18 @@
 interface DynamoTestStation {
-  testStationId: string
-  testStationAddress: string
-  testStationContactNumber: string
-  testStationEmails: string
-  testStationGeneralNotes: string
-  testStationLongitude: string
-  testStationLatitude: string
-  testStationName: string
-  testStationPNumber: string
-  testStationPostcode: string
-  testStationStatus: string
-  testStationTown: string
-  testStationType: string
+  testStationId: string;
+  testStationAccessNotes: string;
+  testStationAddress: string;
+  testStationContactNumber: string;
+  testStationEmails: string;
+  testStationGeneralNotes: string;
+  testStationLongitude: string;
+  testStationLatitude: string;
+  testStationName: string;
+  testStationPNumber: string;
+  testStationPostcode: string;
+  testStationStatus: string;
+  testStationTown: string;
+  testStationType: string;
 }
 
 export { DynamoTestStation };
