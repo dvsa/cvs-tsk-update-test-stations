3d6954fc340f110f98630ece22ff007ddba75fc9
Steven.Pears <steven.pears@live.co.uk>
2022-09-21 15:58:44 +0100

    update dynamo put test

--- tests/unit/handler.test.ts
+++ tests/unit/handler.test.ts
@@ -1,13 +1,20 @@
-import { MemberDetail } from 'aws-sdk/clients/detective';
+import MemberDetails from '../../src/aad/MemberDetails';
 import IDynamoRecord from '../../src/dynamo/IDynamoRecord';
 import { handler } from '../../src/handler';
 
 // has to be 'var' as jest "hoists" execution behind the scenes and let/const cause errors
-var mockMemberDetails:jest.Mock; // eslint-disable-line no-var
-var mockDynamoRecords:jest.Mock; // eslint-disable-line no-var
+/* eslint-disable no-var */
+var mockMemberDetails:jest.Mock;
+var mockDynamoRecords:jest.Mock;
+var mockDynamoPut:jest.Mock;
+var emptyMemberDetails = true;
+/* eslint-enable no-var */
 
 jest.mock('../../src/aad/getMemberDetails', () => {
-  mockMemberDetails = jest.fn().mockResolvedValue(new Array<MemberDetail>());
+  mockMemberDetails = jest.fn().mockResolvedValue(emptyMemberDetails ? new Array<MemberDetails>() : [<MemberDetails>{
+    displayName: 'Test User',
+    userPrincipalName: 'test@email.com',
+  }]);
   return { getMemberDetails: mockMemberDetails };
 });
 
@@ -16,9 +23,25 @@ jest.mock('../../src/dynamo/getDynamoRecords', () => {
   return { getDynamoMembers: mockDynamoRecords };
 });
 
+jest.mock('aws-sdk', () => {
+  mockDynamoPut = jest.fn().mockImplementation(() => ({ promise: jest.fn().mockResolvedValue(<AWS.DynamoDB.DocumentClient.PutItemOutput>{}) }));
+  class FakeDynamoDb {
+    put = mockDynamoPut;
+  }
+
+  const AWS = {
+    DynamoDB: {
+      DocumentClient: FakeDynamoDb,
+    },
+  };
+
+  return AWS;
+});
+
 describe('Handler', () => {
   afterEach(() => {
     jest.clearAllMocks();
+    emptyMemberDetails = true;
   });
 
   it('should request member details', async () => {
@@ -29,5 +52,18 @@ describe('Handler', () => {
   it('should request dynamo details', async () => {
     await handler();
     expect(mockDynamoRecords).toBeCalledTimes(1);
+  });
+
+  it('should run a dynamo put for an active record', async () => {
+    emptyMemberDetails = false;
+    await handler();
+    expect(mockDynamoPut).toBeCalledWith({
+      TableName: '',
+      Item: {
+        resourceType: { S: 'USER' },
+        resourceKey: { S: 'test@email.com' },
+        name: { S: 'Test User' },
+      },
+    });
   });
 });
