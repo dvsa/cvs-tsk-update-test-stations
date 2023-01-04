cc53126220b1d050390cb39f5d4bc8b3a0f77f94
Nathan Brown <nathan.brown@bjss.com>
2023-01-04 12:35:34 +0000

    fix(cb2-0001): remove un used env var

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,8 @@
 export default {
   aad: {
     Secret: process.env.AAD_SECRET || '',
-    authorityId: process.env.AAD_AUTHORITY_ID || '',
     baseUrl: process.env.AAD_BASE_URL || 'https://graph.microsoft.com/',
-    groupId: process.env.AAD_TESTER_GROUP_ID || ''
+    groupId: process.env.AAD_TESTER_GROUP_ID || '',
   },
   aws: {
     dynamoTable: process.env.DYNAMO_TABLE || '',
