41acb4ad17a970b52d47d24119e424902e39a2d3
Nathan Brown <nathan.brown@bjss.com>
2023-01-05 12:53:47 +0000

    fix(cb2-0001): update name of env var

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
 export default {
   aad: {
-    Secret: process.env.AAD_SECRET || '',
     baseUrl: process.env.AAD_BASE_URL || 'https://graph.microsoft.com/',
     groupId: process.env.AAD_TESTER_GROUP_ID || '',
   },
   aws: {
+    Secret: process.env.AAD_CLIENT_SECRET_NAME || '',
     dynamoTable: process.env.DYNAMO_TABLE || '',
   },
   logger: {
