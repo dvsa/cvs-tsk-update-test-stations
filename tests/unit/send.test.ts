96fc958f0cfa18b78230151f465bbf1f881254f1
Shivangi Das <shivangi.das7@gmail.com>
2024-04-30 10:09:34 +0100

    test(cb2-11276): update test to old format

--- tests/unit/send.test.ts
+++ tests/unit/send.test.ts
@@ -1,16 +1,28 @@
-import { EventBridgeClient, PutEventsCommand } from '@aws-sdk/client-eventbridge';
+import {
+  EventBridgeClient, PutEventsCommand, PutEventsCommandInput, PutEventsCommandOutput, PutEventsResultEntry,
+} from '@aws-sdk/client-eventbridge';
 import { mockClient } from 'aws-sdk-client-mock';
 import { sendModifiedTestStations } from '../../src/eventbridge/send';
 import { SendResponse } from '../../src/eventbridge/SendResponse';
 import { DynamoTestStation } from '../../src/crm/DynamoTestStation';
 
 const mockEventBridge = mockClient(EventBridgeClient);
-mockEventBridge.on(PutEventsCommand).resolves({});
+mockEventBridge.on(PutEventsCommand).callsFake(
+  (params: PutEventsCommandInput): PutEventsCommandOutput => {
+    const mPutEventsResponse: PutEventsCommandOutput = {
+      FailedEntryCount: 0,
+      Entries: Array<PutEventsResultEntry>(params.Entries.length),
+      $metadata: undefined,
+    };
+    if (params.Entries[0].Detail === JSON.stringify({ testStationId: 'Error', testStationName: 'Error' })) {
+      throw new Error('Oh no!');
+    } else {
+      return mPutEventsResponse;
+    }
+  },
+);
 describe('Send events', () => {
   describe('Events sent', () => {
-    beforeEach(() => {
-      mockEventBridge.reset();
-    });
     it('GIVEN one event to send WHEN sent THEN one event is returned.', async () => {
       const mTestStations = Array<DynamoTestStation>(1);
       const mSendResponse: SendResponse = { SuccessCount: 1, FailCount: 0 };
@@ -27,7 +39,6 @@ describe('Send events', () => {
       const mTestStations = Array<DynamoTestStation>(6);
       const errorDynamoTestStation = <DynamoTestStation>(<unknown>{ testStationId: 'Error', testStationName: 'Error' });
       mTestStations[0] = errorDynamoTestStation;
-      mockEventBridge.on(PutEventsCommand).rejectsOnce(new Error('Oh No!'));
       const mSendResponse: SendResponse = { SuccessCount: 5, FailCount: 1 };
       await expect(sendModifiedTestStations(mTestStations)).resolves.toEqual(mSendResponse);
     });
